2248619741bada81d5a835ff083a33db8e82f3b0
Anitha <er.anithamohan@gmail.com>
2026-09-22 11:20:52 +0530

    Updated command line to launch document

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
+  //launch PW documnet
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
